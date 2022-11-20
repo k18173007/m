fedff379bd760d740dc33de1bcf308e601e4595a
k18173007 <kk.web.007@gmail.com>
2022-11-20 05:36:27 +0530

    a

---
+++ kkl/src/components/Test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavBar } from '@bootstrap'
+import { Flex } from '@chakra-ui/react'
+const test = () => {
+    return ({
+            /* <div className = 't1' >
+                    <div className = 't2'id = 'k1' ></div>
+                    <div className = 't2'id = 'k2' ></div> 
+                    <div className = 't2'id = 'k3' ></div> 
+                    </div > </div> */
+        }
+        // <NavBar/>
+
+    )
+}
+
+export default test
